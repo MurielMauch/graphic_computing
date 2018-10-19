387e388cb0899243d6f01810a83bfa564f19ac51
MurielMauch <murielmauch@gmail.com>
2018-10-19 16:17:12 -0300

    atividade 2: a e b incompletas

--- exemplo_1.c
+++ exemplo_1.c
@@ -14,13 +14,22 @@ int main(int argc, char ** argv) {
     hobject * poligono1, * poligono2, * poligono3, * poligono4;
     
     SetWorld(-20, 10, -20, 15); 
-    dispositivo = CreateBuffer(640,480);
+    dispositivo = CreateBuffer(1024,768);
+
+    /**
+     *  o tamanho da janela é independente do tamanho da viewport
+     *  view = (viewport *)malloc(sizeof(viewport));
+     *  view->xmin = 0;
+     *  view->xmax = 1024;
+     *  view->ymin = 0;
+     *  view->ymax = 768; 
+     **/
 
     view = (viewport *)malloc(sizeof(viewport));
     view->xmin = 0;
-    view->xmax = 640;
+    view->xmax = 1024;
     view->ymin = 0;
-    view->ymax = 480;
+    view->ymax = 768;
 
     palheta = CreatePalette(6);  
     SetColor(0,0,0,palheta);
@@ -28,34 +37,33 @@ int main(int argc, char ** argv) {
     SetColor(0,1,0,palheta);
     SetColor(0,0,1,palheta);
     SetColor(1,1,1,palheta);
+
+    poligono1 = HCreateObject(7);
+    poligono2 = HCreateObject(3);
+
+    HSetObject(HSetPoint(-6.0,-1.0,2), poligono1);
+    HSetObject(HSetPoint(-11.0,-11.0,2), poligono1);
+    HSetObject(HSetPoint(-9.0,-11.0,2), poligono1);
+    HSetObject(HSetPoint(-8.0,-9.0,2), poligono1);
+    HSetObject(HSetPoint(-4.0,-9.0,2), poligono1);
+    HSetObject(HSetPoint(-3.0,-11.0,2), poligono1);
+    HSetObject(HSetPoint(-1.0,-11.0,2), poligono1);
+
+    HSetObject(HSetPoint(-8.0,-8.0,2), poligono2);
+    HSetObject(HSetPoint(-4.0,-8.0,2), poligono2);
+    HSetObject(HSetPoint(-6.0,-5.0,2), poligono2);
     
-    poligono1 = HCreateObject(5);
-    poligono2 = HCreateObject(4);
-    poligono3 = HCreateObject(4);
-    
-    HSetObject(HSetPoint(-9.0,-8.0,1), poligono1);
-    HSetObject(HSetPoint(-7.0,-3.0,1), poligono1);
-    HSetObject(HSetPoint(-4.0,-4.0,1), poligono1);
-    HSetObject(HSetPoint(-3.0,-6.0,1), poligono1);
-    HSetObject(HSetPoint(-6.0,-9.0,1), poligono1);
-    
-    HSetObject(HSetPoint(-6.0,-2.0,3), poligono2);
-    HSetObject(HSetPoint(-1.0,-2.0,3), poligono2);
-    HSetObject(HSetPoint(-1.0,-6.0,3), poligono2);
-    HSetObject(HSetPoint(-6.0,-6.0,3), poligono2);
+    /*
+    poligono3 = Skew(poligono1,-1,0);
+    poligono4 = Skew(poligono2,-1,0);
+    */
 
-    HSetObject(HSetPoint(0.0,4.0,2), poligono3);
-    HSetObject(HSetPoint(3.0,4.0,2), poligono3);
-    HSetObject(HSetPoint(3.0,0.0,2), poligono3);
-    HSetObject(HSetPoint(0.0,0.0,2), poligono3);
+    // float xmin, float xmax, float ymin, float ymax
+    // janela = CreateWindow(-15.0,2.0,-15.0,2.0);
+    janela = CreateWindow(-20.0,12.0,-20.0,12.0);
 
-    poligono4 = Skew(poligono3,-1,0);
-
-    janela = CreateWindow(-10.0,5.0,-10.0,5.0);
-
-    DrawObject(poligono1,janela,dispositivo,view);
-    DrawObject(poligono2,janela,dispositivo,view);
-    DrawObject(poligono4,janela,dispositivo,view);
+    DrawObject(poligono1,janela,dispositivo,view);  // exterior
+    DrawObject(poligono2,janela,dispositivo,view);  // interior
     
     Dump2X(dispositivo,palheta);
     
