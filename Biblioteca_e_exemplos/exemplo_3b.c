d84adb4265ee747a94e0060b5669b1bc41ab54d7
JessicaDias <jcarolinedias1@gmail.com>
2018-11-10 22:59:17 +0000

    Atividade 3 parte 2 - Questões A e B

--- Biblioteca_e_exemplos/exemplo_3b.c
+++ Biblioteca_e_exemplos/exemplo_3b.c
@@ -1,7 +1,7 @@
 // Para executar:
 // gcc -c cg2d.c
-// gcc exemplo_3a.c -o exemplo_3a cg2d.o -lm -lX11
-// ./exemplo_3a
+// gcc exemplo_3b.c -o exemplo_3b cg2d.o -lm -lX11
+// ./exemplo_3b
 
 #include "cg2d.h"
 
@@ -25,6 +25,7 @@ int main(int argc, char ** argv) {
   
   poligono1 = CreateObject(5);
   poligono2 = CreateObject(4);
+  poligono3 = CreateObject(4);
   
   SetObject(SetPoint(-9.0,-8.0,1,1), poligono1);
   SetObject(SetPoint(-7.0,-3.0,1,1), poligono1);
@@ -36,28 +37,30 @@ int main(int argc, char ** argv) {
   SetObject(SetPoint(-1.0,-2.0,1,3), poligono2);
   SetObject(SetPoint(-1.0,-6.0,1,3), poligono2);
   SetObject(SetPoint(-6.0,-6.0,1,3), poligono2);
+
+  SetObject(SetPoint(-6.0,-2.0,1,3), poligono3);
+  SetObject(SetPoint(-1.0,-2.0,1,3), poligono3);
+  SetObject(SetPoint(-1.0,-6.0,1,3), poligono3);
+  SetObject(SetPoint(-6.0,-6.0,1,3), poligono3);
   
-  // Rotação do poligono1
-  poligono3 = RotateObj(poligono1, 75);
-  /* O efeito colateral da rotação é a translação do objeto, pois a rotação
-  ocorre em torno da origem e, neste caso, o objeto nao está na origem */
-
-  janela = CreateWindow(-6.0, -5.0, 0.0, 0.0);
-  porta = CreateViewPort(215, 95 , 470, 340); // 300x200
-
-  /* Para que os três objetos possam ser visualizados corretamente e
-  sem recortes na viewport considerada, é necessário realizar as
-  operações de translação e escalonamento */
-
   // Translação para centralizar a figura 
-  poligono1 = TranslateObj(poligono1, -8.5, -3.0);
-  poligono2 = TranslateObj(poligono2, -8.5, -3.0);
-  poligono3 = TranslateObj(poligono3, -8.5, -3.0);
+  poligono1 = TranslateObj(poligono1, 5.0, 5.0);
+  poligono2 = TranslateObj(poligono2, 5.0, 5.0);
+
+  // Traslação do polinogo3 para a origem
+  poligono3 = TranslateObj(poligono3, 6.0, 6.0);
+  // Rotação do poligono3
+  poligono3 = RotateObj(poligono3, 45);
+  // Translação do poligono3 para as coordenadas iniciais
+  poligono3 = TranslateObj(poligono3, -1.0, -1.0);
 
   // Escalonamento
-  poligono1 = ScaleObj(poligono1, 0.3, 0.3);
-  poligono2 = ScaleObj(poligono2, 0.3, 0.3);
-  poligono3 = ScaleObj(poligono3, 0.3, 0.3);
+  poligono1 = ScaleObj(poligono1, 0.2, 0.2);
+  poligono2 = ScaleObj(poligono2, 0.2, 0.2);
+  poligono3 = ScaleObj(poligono3, 0.2, 0.2);
+
+  janela = CreateWindow(-2.0, -2.0, 2.0, 2.0);
+  porta = CreateViewPort(215, 95, 470, 340); // 300x200
   
   DrawObject(poligono1,janela,porta,monitor,1);
   DrawObject(poligono2,janela,porta,monitor,3);
